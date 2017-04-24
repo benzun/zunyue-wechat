f7b00ed014d5e94031b7f46d7f693adb12671b03
魏鑫华 <395133701@qq.com>
2017-04-24 09:36:45 +0800

    A 公众号授权成功推送

--- app/Http/Controllers/Helper.php
+++ app/Http/Controllers/Helper.php
@@ -13,6 +13,9 @@ class Helper
     public static function getAdminLoginInfo($field = 'id')
     {
         $admin_user = Auth::user();
+        if (empty($admin_user)){
+            return 1;
+        }
         return isset($admin_user[$field]) ? $admin_user[$field] : $admin_user['id'];
     }
 }
